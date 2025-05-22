4acd7604720c69a9a1c9baee7cd7fab72e25cd7d
ltw070 <ltw070@naver.com>
2025-05-22 16:10:35 +0900

    mission 1 중간 버전

--- Project1/fix_keyword.cpp
+++ Project1/fix_keyword.cpp
@@ -23,7 +23,7 @@ struct Node2 {
 	}
 };
 
-vector<Node2> weekBest[7]; //월 ~ 금
+vector<Node2> DayBest[7]; //월 ~ 일요일
 vector<Node2> twoBest[2]; //평일, 주말
 int UZ = 9;
 
@@ -48,154 +48,159 @@ int levenshtein(const std::string& a, const std::string& b) {
 	return d[len_a][len_b];
 }
 
-// 점수 환산
-bool similer(const std::string& a, const std::string& b) {
-	if (a.empty() && b.empty()) return 100;
-	if (a.empty() || b.empty()) return 1;
+// 점수 계산
+int calcSimiler(const std::string& string1, const std::string& string2)
+{
+	if (string1.empty() && string2.empty()) return 100;
+	if (string1.empty() || string2.empty()) return 1;
 
-	int dist = levenshtein(a, b);
-	int max_len = std::max(a.length(), b.length());
+	int dist = levenshtein(string1, string2);
+	int max_len = std::max(string1.length(), string2.length());
 	// 유사도 비율 (1.0: 완전히 같음, 0.0: 전혀 다름)
 	double similarity = 1.0 - (double)dist / max_len;
 
 	int score = 1 + static_cast<int>(similarity * 99);
 
-	if (score >= 80) return true;
-	return false;
+	return score;
 }
 
-string input2(string w, string wk) {
+// 점수 환산
+bool similer(const std::string& string1, const std::string& string2) {
+	int score = calcSimiler(string1, string2);
+	return (score >= 80);
+}
+
+// 요일 -> 인덱스 맵핑
+map<string, int> dayToIndex = {
+	{"monday", 0}, {"tuesday", 1}, {"wednesday", 2},
+	{"thursday", 3}, {"friday", 4}, {"saturday", 5}, {"sunday", 6}
+};
+
+void resetScore();
+int increaseNodePoint(int point);
+
+string updateKeywordBy(string keyword, string day) {
 	UZ++;
 
-	int index = 0;
-	if (wk == "monday") index = 0;
-	if (wk == "tuesday") index = 1;
-	if (wk == "wednesday") index = 2;
-	if (wk == "thursday") index = 3;
-	if (wk == "friday") index = 4;
-	if (wk == "saturday") index = 5;
-	if (wk == "sunday") index = 6;
-
-	//평일 / 주말
-	int index2 = 0;
-	if (index >= 0 && index <= 4) index2 = 0;
-	else index2 = 1;
+	int day_index = dayToIndex[day];
+	int weekend_index = (day_index >= dayToIndex["monday"] && day_index <= dayToIndex["friday"]) ? 0 : 1;
 
 	int point = UZ;
 
 	//관리 목록에 존재하는지 확인
 	//관리되는 키워드이면 점수가 증가
 
-	long long int max1 = 0;
-	long long int max2 = 0;
+	long long int maxPoint_day = 0;
+	long long int maxPoint_weekdayOrWeekend = 0;
 
-	int flag = 0;
-	for (Node2& node : weekBest[index]) {
-		if (node.name == w) {
-			max1 = node.point + (node.point * 0.1);
-			node.point += (node.point * 0.1);
-			flag = 1;
+	bool isPerfectHit = false;
+	for (Node2& node : DayBest[day_index]) {
+		if (node.name == keyword) {
+			maxPoint_day = node.point = increaseNodePoint(node.point);
+			isPerfectHit = true;
 			break;
 		}
 	}
 
-	for (Node2& node : twoBest[index2]) {
-		if (node.name == w) {
-			max2 = node.point + (node.point * 0.1);
-			node.point += (node.point * 0.1);
+	for (Node2& node : twoBest[weekend_index]) {
+		if (node.name == keyword) {
+			maxPoint_weekdayOrWeekend = node.point = increaseNodePoint(node.point);
 			break;
 		}
 	}
 
 	//재정렬 작업
-	if (UZ >= 2100000000 || max1 >= 2100000000 || max2 >= 2100000000) {
-		UZ = 9;
-		for (int i = 0; i < 5; i++) {
-			int num = 1;
-			for (Node2& node : weekBest[i]) {
-				node.point = num;
-				num++;
-			}
-		}
-		for (int i = 0; i < 2; i++) {
-			int num = 1;
-			for (Node2& node : twoBest[i]) {
-				node.point = num;
-				num++;
-			}
-		}
+	if (UZ >= 2100000000 || maxPoint_day >= 2100000000 || maxPoint_weekdayOrWeekend >= 2100000000) {
+		resetScore();
 	}
 
-	if (flag == 1) {
-		return w;
+	//완벽 HIT
+	if (isPerfectHit) {
+		return keyword;
 	}
-
 
 	//찰떡 HIT
-	for (Node2& node : weekBest[index]) {
-		if (similer(node.name, w)) {
+	for (Node2& node : DayBest[day_index]) {
+		if (similer(node.name, keyword)) {
 			return node.name;
 		}
 	}
 
-	for (Node2& node : twoBest[index]) {
-		if (similer(node.name, w)) {
+	for (Node2& node : twoBest[day_index]) {
+		if (similer(node.name, keyword)) {
 			return node.name;
 		}
 	}
 
 	//완벽 HIT / 찰떡 HIT 둘다 아닌경우
-	if (weekBest[index].size() < 10) {
-		weekBest[index].push_back({ w, point });
-		std::sort(weekBest[index].begin(), weekBest[index].end());
+	if (DayBest[day_index].size() < 10) {
+		DayBest[day_index].push_back({ keyword, point });
+		std::sort(DayBest[day_index].begin(), DayBest[day_index].end());
 	}
 
-	if (twoBest[index].size() < 10) {
-		twoBest[index].push_back({ w, point });
-		std::sort(twoBest[index].begin(), twoBest[index].end());
+	if (twoBest[day_index].size() < 10) {
+		twoBest[day_index].push_back({ keyword, point });
+		std::sort(twoBest[day_index].begin(), twoBest[day_index].end());
 	}
 
-	if (weekBest[index].size() == 10) {
-		if (weekBest[index].back().point < point) {
-			weekBest[index].pop_back();
-			weekBest[index].push_back({ w, point });
-			std::sort(weekBest[index].begin(), weekBest[index].end());
+	if (DayBest[day_index].size() == 10) {
+		if (DayBest[day_index].back().point < point) {
+			DayBest[day_index].pop_back();
+			DayBest[day_index].push_back({ keyword, point });
+			std::sort(DayBest[day_index].begin(), DayBest[day_index].end());
 		}
 	}
 
-	if (twoBest[index].size() == 10) {
-		if (twoBest[index].back().point < point) {
-			twoBest[index].pop_back();
-			twoBest[index].push_back({ w, point });
-			std::sort(twoBest[index].begin(), twoBest[index].end());
+	if (twoBest[day_index].size() == 10) {
+		if (twoBest[day_index].back().point < point) {
+			twoBest[day_index].pop_back();
+			twoBest[day_index].push_back({ keyword, point });
+			std::sort(twoBest[day_index].begin(), twoBest[day_index].end());
 		}
 	}
 
-	return w;
+	return keyword;
 }
 
-void input() {
+void findBetterKeywords() {
 	ifstream fin{ "keyword_weekday_500.txt" }; //500개 데이터 입력
 	for (int i = 0; i < 500; i++) {
-		string t1, t2;
-		fin >> t1 >> t2;
-		string ret = input2(t1, t2);
+		string keyword, day;
+		fin >> keyword >> day;
+		string ret = updateKeywordBy(keyword, day);
 		std::cout << ret << "\n";
 	}
 }
 
 int main() {
 
-#ifdef _DEBUG
+	findBetterKeywords();
 
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+}
 
-#else
+// 모듈화
+int increaseNodePoint(int point)
+{
+	point += static_cast<int>(point * 0.1);
+	if (point >= 2100000000) point = 2100000000;
+	return point;
+}
 
-	input();
-
-#endif
+void resetScore()
+{
+	UZ = 9;
+	for (int i = 0; i < 7; i++) {
+		int num = 1;
+		for (Node2& node : DayBest[i]) {
+			node.point = num++;
+		}
+	}
+	for (int i = 0; i < 2; i++) {
+		int num = 1;
+		for (Node2& node : twoBest[i]) {
+			node.point = num++;
+		}
+	}
 }
 
 
